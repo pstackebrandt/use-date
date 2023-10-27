c4424cd09e38bed1a391f3c3b2520a516992f675
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-10-27 22:44:27 +0200

    Get hours from birth until school: fix birthday

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Get time until school
-let birthdayMSec = (new Date("2000-03-24")).getMilliseconds();
+// Get hours from birth until school
+let birthdayMSec = (new Date("2000-03-24")).getTime();
 let firstDayInSchoolMSec = (new Date("2008-09-01")).getTime();
 let timeUntilSchoolMSec = firstDayInSchoolMSec - birthdayMSec;
 console.log(timeUntilSchoolMSec / 1000 / 60 / 60);
